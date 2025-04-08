fec21f399c49fb46fa8bbaa24fd4f70e2860c7a3
abghifareihand <info.abghifareihan@gmail.com>
2025-04-08 11:41:13 +0700

    fix: update urk endpoint

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,4 @@
 class AppConstants {
-  static const String baseUrl = 'http://192.168.1.8:8000';
-  // static const String baseUrl = 'https://3291-180-244-23-31.ngrok-free.app';
+  static const String baseUrl = 'https://lemburcerdas.my.id';
   static const String photoUrl = '$baseUrl/storage';
 }
